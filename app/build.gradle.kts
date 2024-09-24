b0246c4c712dc0d548c56a75f12e38b54c3e6adb
vicenzo.castelli <vcastelli2004@gmail.com>
2024-09-23 22:29:14 -0300

    final aula

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,9 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.activity:activity-ktx:1.7.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
